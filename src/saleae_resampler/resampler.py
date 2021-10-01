6df82b102bff628f946cdcfd3ef9b9e0c8b721e4
Joni Kähärä <joni.kahara@async.fi>
2021-10-01 22:56:25 +0300

    stats stub

--- src/saleae_resampler/resampler.py
+++ src/saleae_resampler/resampler.py
@@ -3,7 +3,7 @@
 import logging
 import datetime
 from dataclasses import dataclass, field
-from typing import TextIO, BinaryIO
+from typing import Dict, TextIO, BinaryIO
 
 from saleae_resampler.reader import Reader
 
@@ -30,10 +30,24 @@ class Resampler:  # pylint: disable=R0902
     def resample(self) -> int:
         """Resample"""
 
+        stats: Dict[str, Dict[str, float]] = {
+            "input": {
+                "min": float("inf"),
+                "max": float("-inf"),
+            },
+            "bytes": {
+                "0": int(0),
+                "1": int(0),
+            },
+        }
+
         print(f"{self.inputstream} {self.outputstream} {self.channel} {self.samplerate}")
         reader = Reader(inputstream=self.inputstream, channel=self.channel)
 
         for (timestamp, sample) in reader.read():
             print(f"{timestamp} {sample}")
+
+        print(f"""input:\n  min:\t{stats["input"]["min"]}\n  max:\t{stats["input"]["max"]}""")
+        print(f"""bytes:\n    0:\t{stats["bytes"]["0"]}\n    1:\t{stats["bytes"]["1"]}""")
 
         return 0
